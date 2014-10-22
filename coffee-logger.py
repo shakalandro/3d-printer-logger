b84fc55d185345c6dd8b3fcc8c4765d9ec59459b
Roy McElmurry <shakalandro@gmail.com>
2014-10-22 16:46:34 -0700

    fix bug where we would block on badge reading

--- coffee-logger.py
+++ coffee-logger.py
@@ -5,6 +5,7 @@ import time, json, httplib, os, atexit, thread
 import RPi.GPIO as io
 import time
 import datetime
+import select
 from evdev import InputDevice, list_devices, categorize, ecodes
 from parse_rest.connection import register
 from parse_rest.datatypes import Object, Function
@@ -19,7 +20,7 @@ KEYCODES = {
 }
 
 ALLOW_TIME_BUMPS = True
-BUMP_INCREMENT = 60 * 60 * 1;
+BUMP_INCREMENT = 60 * 60 * 1
 
 class PrinterLog(Object):
     pass
@@ -51,19 +52,28 @@ def cleanup(device):
 def log(badgenum, sequence_num, allowed):
         ts = time.time()
         ts_str = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
-        print("%d: %s, %d\n" % (badgenum, ts_str, sequence_num))
 	newlog = PrinterLog(badge_num=badgenum, time=ts, time_str=ts_str, sequence_number=sequence_num, allowed_time=allowed)
 	newlog.save()
 
 def get_input(device):
 	val = ''
-	for event in device.read_loop():
+        none_time_start = 0
+	while True:
+                event = device.read_one()
+                if event is None:
+                        if none_time_start == 0:
+                            none_time_start = time.time()
+                        if time.time() - none_time_start > 1:
+                            break
+                        else:
+                            continue
+                none_time_start = 0
 		ev = categorize(event)
 		if event.type == ecodes.EV_KEY and ev.keystate == ev.key_down:
 			if ev.keycode == 'KEY_ENTER':
 				break
 			val += KEYCODES[ev.keycode]
-	return val
+	return val if val != '' else None 
 
 if __name__ == "__main__":
 	device = get_scanner_device()
@@ -75,22 +85,24 @@ if __name__ == "__main__":
         allowed = 0
         sequence_num = 0
 	while True:
-		try: 
-                        badgenum = int(get_input(device))
-                        if allowed == 0:
-                                io.output(POWER_PIN, True)
-                                started = time.time()
-                        allowed += BUMP_INCREMENT    
-                        sequence_num += 1
-                except ValueError:
-                        time.sleep(0.1)
                 if (time.time() - started) < allowed:
                         io.output(POWER_PIN, True)
-                        thread.start_new_thread(log, (badgenum,sequence_num,allowed))
                 else:
                         io.output(POWER_PIN, False)
                         allowed = 0
                         started = 0
                         sequence_num = 0
-        
+                try:    
+                        badgenum = int(get_input(device))
+                        if badgenum is not None:
+                                if allowed == 0:
+                                        io.output(POWER_PIN, True)
+                                        started = time.time()
+                                allowed += BUMP_INCREMENT    
+                                sequence_num += 1
+                                thread.start_new_thread(log, (badgenum,sequence_num,allowed))
+                except Exception:
+                        time.sleep(0.1)
+                time.sleep(0.1)
+                        
 
